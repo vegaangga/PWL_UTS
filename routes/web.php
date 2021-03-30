72ce288011cfbeafd677c6ad3b96a84d81bbbdbf
vegaangga <vegaangga27@gmail.com>
2021-03-30 16:28:50 +0700

    Mengedit file web.php agar menambahkan route resource dan route get untuk fungsi search

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BarangController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('barang', BarangController::class);
+
+Route::get('/search',[BarangController::class, 'cari']);
