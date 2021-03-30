847b609b390c86b138f46edb2067b86985abd133
vegaangga <vegaangga27@gmail.com>
2021-03-30 16:26:42 +0700

    memodifikasi barang model untuk mengatur primarykey, table, dan yang lainnya

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     use HasFactory;
+
+    protected $table= 'barang';
+    protected $primaryKey='kode_barang';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = [
+        'id_barang',
+        'kode_barang',
+        'nama_barang',
+        'kategori_barang',
+        'harga',
+        'qty',
+       ];
 }
